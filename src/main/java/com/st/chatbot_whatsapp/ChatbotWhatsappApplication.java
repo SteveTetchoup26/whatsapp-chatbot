86fbdf69d0f3ed09fa158f235f28956a238a7738
Steve Tetchoup <stevetcthoup20@gmail.com>
2025-11-08 15:32:13 +0100

    refactor:"edit application.properties"

--- src/main/java/com/st/chatbot_whatsapp/ChatbotWhatsappApplication.java
+++ src/main/java/com/st/chatbot_whatsapp/ChatbotWhatsappApplication.java
@@ -1,5 +1,6 @@
 package com.st.chatbot_whatsapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,4 @@ public class ChatbotWhatsappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ChatbotWhatsappApplication.class, args);
 	}
-
 }
